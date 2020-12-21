af73ff21dffee4927ed73a561b49c293ebf37967
rodrigojpt <rodrigojpereirat@gmail.com>
2020-12-21 01:45:08 -0500

    added resume

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 			<Route path='/about' exact component={About} />
 			<Route
 				path='/:id'
+				exact
 				render={(props) => <ProjectDetails id={props.match.params.id} />}
 			/>
 			<Icons />
--- src/Header.js
+++ src/Header.js
@@ -8,8 +8,14 @@ const Header = () => {
 				<h3>{'</>'}</h3>
 			</Link>
 			<nav>
-				<Link to='/about'>About Me</Link>
 				<Link to='/'>Portfolio</Link>
+				<Link to='/about'>About Me</Link>
+				<a
+					href='https://rodrigojpt.github.io/portfolio/media/resume.pdf'
+					target='_blank'
+					rel='noreferrer'>
+					Resume
+				</a>
 			</nav>
 		</header>
 	);
