8edbebd3971056c77cbe030c76d463d89a3115f2
rodrigojpt <rodrigojpereirat@gmail.com>
2021-01-06 01:47:26 -0500

    updated projects json

--- src/Icons.js
+++ src/Icons.js
@@ -7,16 +7,16 @@ const Icons = () => {
 				href='mailto:rodrigojpereirat@gmail.com'
 				target='_blank'
 				rel='noreferrer'>
-				<i class='fas fa-envelope-square'></i>
+				<i className='fas fa-envelope-square'></i>
 			</a>
 			<a href='https://github.com/RodrigoJPT' target='_blank' rel='noreferrer'>
-				<i class='fab fa-github-square'></i>
+				<i className='fab fa-github-square'></i>
 			</a>
 			<a
 				href='https://www.linkedin.com/in/rodrigojpt/'
 				target='_blank'
 				rel='noreferrer'>
-				<i class='fab fa-linkedin'></i>
+				<i className='fab fa-linkedin'></i>
 			</a>
 		</div>
 	);
