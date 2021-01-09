f50ba4ac20c3a70f854b442a5feacda2acf50535
rodrigojpt <rodrigojpereirat@gmail.com>
2021-01-08 19:12:32 -0500

    changed about me photo url

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ const About = () => {
 		<div className='about'>
 			<div className='about-container'>
 				<img
-					src='https://rodrigojpt.github.io/portfolio/media/myself.jpeg'
+					src='https://rodrigojpt.github.io/media/myself.jpeg'
 					alt='Rodrigo outdoors'
 					className='profile-pic'
 				/>
